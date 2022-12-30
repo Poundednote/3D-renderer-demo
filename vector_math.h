54cd313dfe8f6dc826ddd62f5333f0a1e03e431e
Poundedenote <rayyan.naz.02@gmail.com>
2022-12-30 10:14:34 +0000

    commented code and added scalar multiplication function

--- vector_math.h
+++ vector_math.h
@@ -2,14 +2,34 @@
 #include <vector>
 using namespace std;
 
-vector<float> add(vector<float> a, vector<float> b) {
-    if (!a.size() == b.size()) {
+namespace vector_math {
+
+/* takes two vectors both size n and adds them and returns new resutlting vector
+    add([x1, x2...xn-1, xn], [Y1, Y2...yn-1, Yn])
+    returns [x1+y1, x2+y2...Xn-1+Yn-1, Xn+Yn]
+*/
+vector<float> add(vector<float> v1, vector<float> v2) {
+    if (!v2.size() == v2.size()) {
         throw invalid_argument("vectors have to be the same size");
     } else {
-        vector<float> result;
-        for (int i = 0; i < a.size(); i++) {
-            result.push_back(a[i] + b[i]);
+        vector<float> v3;
+        for (int i = 0; i < v1.size(); i++) {
+            v3.push_back(v1[i] + v2[i]);
         }
-        return result;
+        return v3;
     }
 }
+
+/* takes vector [X1, X2...Xn-1, Xn] and multiplies by scalar value such that the
+   result is [SX1, SX2,SXn-1, SXn]
+    */
+
+vector<float> scalar_mult(vector<float> v1, float s) {
+    vector<float> v2;
+    for (int i = 0; i < v1.size(); i++) {
+        v2.push_back(v1[i] * s);
+    }
+    return v2;
+}
+
+}  // namespace vector_math
