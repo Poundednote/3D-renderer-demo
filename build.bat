adbcc7aa280a90c5d1c37b5fb327af462e457304
Poundedenote <rayyan.naz.02@gmail.com>
2023-05-04 08:36:12 +0100

    started interpolating vertex colors

--- build.bat
+++ build.bat
@@ -4,4 +4,4 @@ set CommonCompilerFlags= -MT -nologo -GR- -Gm- -EHa- -Oi -Od -WX -W4 -wd4100 -wd
 
 mkdir "build"
 pushd "build"
-cl %CommonCompilerFlags% -DSSE=0 -Od ..\win_particle.cpp User32.lib Gdi32.lib
+cl %CommonCompilerFlags% -DSSE=0 -O2 ..\win_particle.cpp User32.lib Gdi32.lib
